a21f1d2b06c60514a8bcf7dd752331428f47cbd9
Vasylchenko Anton <87824792+Smd16Bi@users.noreply.github.com>
2024-07-16 08:13:28 +0300

    fixed quantity

--- extensions/product-discount/src/run.ts
+++ extensions/product-discount/src/run.ts
@@ -3,6 +3,7 @@ import type {
   FunctionRunResult,
   ProductVariant,
   CartLine,
+  ProductVariantTarget,
 } from "../generated/api";
 import { DiscountApplicationStrategy } from "../generated/api";
 
@@ -24,15 +25,21 @@ function groupByDiscountId(array: any) {
 
 export function run(input: RunInput): FunctionRunResult {
   const groupProductWithDiscount = input.cart.lines.filter((line) => line.discountId?.value);
-
+  
+  console.log(input.cart.lines.length);
+  
+  if (input.cart.lines.length < 2) {
+    return EMPTY_DISCOUNT
+  }
   const groupedData = Object.values(groupByDiscountId(groupProductWithDiscount)).filter((group) => {
     return (group as CartLine[]).length === 2;
   })
 
-  const groupedDataCart = (groupedData[0] as CartLine[]).filter(line => line.quantity === 1).map((line) => {
+  const groupedDataCart = (groupedData[0] as CartLine[]).filter(line => line.quantity === 1).map((line) =>   {
     return {
       productVariant: {
-        id: (line.merchandise as ProductVariant).id,
+        id: String((line.merchandise as ProductVariant).id),
+        quantity: 1
       },
     };
   });
