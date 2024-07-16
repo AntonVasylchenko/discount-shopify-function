703142e9bb14d52b1eeb9382bfb4e48ed9002195
Vasylchenko Anton <87824792+Smd16Bi@users.noreply.github.com>
2024-07-16 06:41:23 +0300

    finish

--- extensions/product-discount/src/run.ts
+++ extensions/product-discount/src/run.ts
@@ -1,21 +1,60 @@
 import type {
   RunInput,
-  FunctionRunResult
+  FunctionRunResult,
+  ProductVariant,
+  CartLine,
 } from "../generated/api";
-import {
-  DiscountApplicationStrategy,
-} from "../generated/api";
+import { DiscountApplicationStrategy } from "../generated/api";
 
 const EMPTY_DISCOUNT: FunctionRunResult = {
   discountApplicationStrategy: DiscountApplicationStrategy.First,
   discounts: [],
 };
 
-type Configuration = {};
+function groupByDiscountId(array: any) {
+  return array.reduce((result, item) => {
+    const key = item?.discountId.value;
+    if (!result[key]) {
+      result[key] = [];
+    }
+    result[key].push(item);
+    return result;
+  }, {});
+}
 
 export function run(input: RunInput): FunctionRunResult {
-  const configuration: Configuration = JSON.parse(
-    input?.discountNode?.metafield?.value ?? "{}"
-  );
-  return EMPTY_DISCOUNT;
-};
+  const groupProductWithDiscount = input.cart.lines.filter((line) => line.discountId?.value);
+
+  const groupedData = Object.values(groupByDiscountId(groupProductWithDiscount)).filter((group) => {
+    return (group as CartLine[]).length === 2;
+  })
+
+  const groupedDataCart = (groupedData[0] as CartLine[]).filter(line => line.quantity === 1).map((line) => {
+    return {
+      productVariant: {
+        id: (line.merchandise as ProductVariant).id,
+      },
+    };
+  });
+
+
+  if (groupedDataCart.length && groupedDataCart.length % 2 === 0) {
+    const result: FunctionRunResult = {
+      discountApplicationStrategy: DiscountApplicationStrategy.First,
+      discounts: [
+        {
+          targets: groupedDataCart,
+          value: {
+            percentage: {
+              value: 10,
+            },
+          },
+          message: "10% OFF",
+        },
+      ],
+    };
+    return result;
+  }
+
+  return EMPTY_DISCOUNT
+}
